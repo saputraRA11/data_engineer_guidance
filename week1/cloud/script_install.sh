c366ea2b0955ae31238525c92dcf27341c6a2f24
saputra <realary27@gmail.com>
2024-07-19 20:46:37 +0700

    adjustment: add automation

--- week1/cloud/script_install.sh
+++ week1/cloud/script_install.sh
@@ -9,13 +9,9 @@ function main() {
     pyenv install -v 3.9
 
     echo "========= install terraform ==========="
-    sudo apt-get install -y gnupg software-properties-common && \
-    wget -O- https://apt.releases.hashicorp.com/gpg | \
-    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null && \
-    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-    sudo tee /etc/apt/sources.list.d/hashicorp.list && \
-    sudo apt-get update && \
-    sudo apt-get install terraform
+    wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    sudo apt update && sudo apt install terraform
 
     echo "========= install docker ==========="
     sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev && \
